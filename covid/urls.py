538ce188c2708e3ca16e2a363097f96806418110
Viraj-creation312 <67023201+Viraj-creation312@users.noreply.github.com>
2021-03-06 21:53:56 +0530

    Update urls.py

--- covid/urls.py
+++ covid/urls.py
@@ -15,10 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-
-s
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('covidapp.urls')),
